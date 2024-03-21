e0ae973cb7b870b64e95815996ae3a0c0bbe2a03
Ahmed Eid <83458048+AhmedEid1998@users.noreply.github.com>
2024-03-21 21:03:39 +0200

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BsDatepickerModule.forRoot()],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
